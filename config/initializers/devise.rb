cdfabbc0fafada3d123f3e52abd390467922f6a4
Darek Charkot <darek.charkot@gmail.com>
2016-03-21 08:21:14 +0100

    Add logout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -238,7 +238,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, 'f8b1eb46e68b2e31415c', '5a55d89344190aa216936ce6a47449dacda97eee', scope: 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
